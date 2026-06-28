4d4307b9499d58a94b4268acae4521992b953a6c
David Lee <41690461+davidseungjin@users.noreply.github.com>
2026-06-27 20:53:10 -0700

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # top-down DP
+        # arr =[-1 for _ in range(n)]
+        # def dfs(i):
+        #     if i >= n:
+        #         return i == n
+        #     if arr[i] != -1:
+        #         return arr[i]
+        #     arr[i] = dfs(i+1) + dfs(i+2)
+        #     return arr[i]
+        # return dfs(0)
+
+        # if n <= 2:
+        #     return n
+        # dp = [0 for _ in range(n+1)]
+        # dp[1] = 1
+        # dp[2] = 2
+        # for i in range(3, n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]
+        # return dp[n]
+
+        # step
+        curr = 1
+        prev = 1
+
+        # since initial prev, curr stands for
+        # way to climb steps zero and one
+        # iterate n-1 times (as it already has answer for 1 step)
+        for i in range(n-1):
+            temp = curr
+            curr = curr + prev
+            prev = temp
+        return curr
